f4833ecd868fe70e489e9e6dfe11cf445999550b
lightnonstop <umichaeledet003@gmail.com>
2023-01-03 11:29:08 +0100

    Code test case fixing

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,6 +11,8 @@ int check_cycle(listint_t *list)
 	listint_t *fast;
 
 	fast = malloc(sizeof(listint_t));
+	if (list == NULL || fast || NULL)
+		return (0);
 	/*
 	 * Moves twice as fast as the list pointer from
 	 * one node to another
