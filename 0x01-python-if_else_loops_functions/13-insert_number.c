42039ede156f46a0a054625ad09f8411dd19de2c
lightnonstop <umichaeledet003@gmail.com>
2023-01-04 12:55:27 +0100

    Error fixed in source file

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,7 +11,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *temp, *new;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL && *head == NULL)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
