c120733a8e7aca8cd72361f44313388f1e37e2dd
lightnonstop <umichaeledet003@gmail.com>
2023-01-05 12:50:32 +0100

    Imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,5 @@ from add_0 import add
 a = 1
 b = 2
 if __name__ == '__main__':
+    # The code in this section runs only if this module is not imported
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
