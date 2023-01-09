0b99902ba7958e14e286f152283aca00dbcbb9b2
lightnonstop <umichaeledet003@gmail.com>
2023-01-09 22:04:59 +0000

    Retrieves an element from a list like in C

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        for i in range(len(my_list)):
+            if idx == i:
+                return my_list[i]
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+        
