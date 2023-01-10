c762bb74b001c4816c8448e8f96d9fa7dfd2e508
lightnonstop <umichaeledet003@gmail.com>
2023-01-10 22:22:32 +0000

    Adds two tuples and error fixed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    arr = []
-    if len(tuple_a) > len(tuple_b):
-        Longest = len(tuple_a)
-    else:
-        Longest = len(tuple_b)
-    for i in range(Longest):
-        if i > len(tuple_a) - 1:
-            arr.append(tuple_b[i])
-        elif i > len(tuple_b) - 1:
-            arr.append(tuple_a[i])
-        else:
-            arr.append(tuple_a[i] + tuple_b[i])
-    return tuple(arr)
+    a = (0, 0)
+    b = (0, 0)
+
+    if len(tuple_a) == 0:
+        a = (0, 0)
+    elif len(tuple_a) == 1:
+        a = (tuple_a[0], 0)
+    elif len(tuple_a) >= 2:
+        a = (tuple_a[0], tuple_a[1])
+
+    if len(tuple_b) == 0:
+        b = (0, 0)
+    elif len(tuple_b) == 1:
+        b = (tuple_b[0], 0)
+    elif len(tuple_b) >= 2:
+        b = (tuple_b[0], tuple_b[1])
+
+    return (a[0] + b[0], a[1] + b[1])
