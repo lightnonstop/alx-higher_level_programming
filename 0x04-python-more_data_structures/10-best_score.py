6db29bb261125459ee41908a8b46d2b9e0b293f6
lightnonstop <umichaeledet003@gmail.com>
2023-01-12 14:20:50 +0000

    Finds the key with the biggest integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    else:
+        bigg_value = max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
+    return bigg_value
